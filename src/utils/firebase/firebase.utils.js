449c7170e50793cd591cf6ead6fe1bc3b3226e53
yerda-zh <yerdaulet.04@gmail.com>
2023-07-19 20:49:54 +0500

    used redux toolkit

--- src/utils/firebase/firebase.utils.js
+++ src/utils/firebase/firebase.utils.js
@@ -21,12 +21,12 @@ import {
 } from 'firebase/firestore';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyDDU4V-_QV3M8GyhC9SVieRTDM4dbiT0Yk',
-  authDomain: 'crwn-clothing-db-98d4d.firebaseapp.com',
-  projectId: 'crwn-clothing-db-98d4d',
-  storageBucket: 'crwn-clothing-db-98d4d.appspot.com',
-  messagingSenderId: '626766232035',
-  appId: '1:626766232035:web:506621582dab103a4d08d6',
+  apiKey: "AIzaSyA9i-97hsl3lMi1QTcR9zwjZfERj8YAucU",
+  authDomain: "clothing-brand-db-a54bc.firebaseapp.com",
+  projectId: "clothing-brand-db-a54bc",
+  storageBucket: "clothing-brand-db-a54bc.appspot.com",
+  messagingSenderId: "842554595923",
+  appId: "1:842554595923:web:41f3ee66504491666884a6"
 };
 
 const firebaseApp = initializeApp(firebaseConfig);
